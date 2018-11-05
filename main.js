bb949a8eb8dd80fba6378764eb1b04c435b3766a
AVT <connect@avtconnect.com>
2018-11-05 12:24:27 +0000

    window frame:false and setMenu(null)

--- main.js
+++ main.js
@@ -10,14 +10,18 @@ function createWindow () {
     width: 1280,
     minWidth: 680,
     height: 720,
-    title: app.getName()
+    title: app.getName(),
+    frame: false
   })
+
+  // no menus
+  win.setMenu(null);
 
   // and load the index.html of the app.
   win.loadFile('index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
